b9974fecbbe024a4f5836d7a141a7ac834cfb527
Jun Hee Han <junheehan@berkeley.edu:cdgit config --global user.email junheehan@berkeley.edu>
2019-06-20 20:56:00 -0700

    Initial Commit

---
+++ ARLearner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARLearner
+//
+//  Created by Jun Hee Han on 6/20/19.
+//  Copyright © 2019 Jun Hee Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
